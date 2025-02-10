cfdb6de6dcba6551f2bb92be247c89f2d5ce9e7d
yamap <yamap.web@gmail.com>
2025-02-11 02:20:39 +0900

    daisyUIの導入とタイマーを作成する準備

---
+++ src/app/components/Timer/index.tsx
@@ -0,0 +1,10 @@
+export function Timer() {
+  return (
+    <div id="container">
+      <div id="pip">
+        <p className="text-9xl">00:00</p>
+        <button className="btn btn-primary">Button</button>
+      </div>
+    </div>
+  );
+}
