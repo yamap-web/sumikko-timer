960006626c7aee5d9ed5f9d467fdb0e2dc3090cf
yamap <yamap.web@gmail.com>
2025-02-19 01:55:38 +0900

    フッターの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import PictureInPictureWindow, {
   usePictureInPicture,
 } from '@/hooks/usePictureInPicture';
 import { useTimer } from '@/hooks/useTimer';
+import { Footer } from '@/components/Footer';
 
 export default function Home() {
   const containerRef = useRef<HTMLDivElement | null>(null);
@@ -16,38 +17,41 @@ export default function Home() {
     useTimer();
 
   return (
-    <div className="grid items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="">
-        <h1 className="font-bold text-5xl mb-4 text-center">
-          すみっこタイマー
-        </h1>
-        <div className="flex space-x-2 mb-3">
-          {!pipWindow ? (
-            <button className="btn btn-link" onClick={handleOpenPipWindow}>
-              画面のすみっこへGo!
-            </button>
-          ) : (
-            <button className="btn btn-link" onClick={handleClosePipWindow}>
-              もどって来ていいよ!
-            </button>
-          )}
-        </div>
-        <div
-          className="rounded-lg h-72 w-[400px] border shadow-lg"
-          ref={containerRef}
-        >
-          <PictureInPictureWindow pipWindow={pipWindow}>
-            <Timer
-              timeLeft={timeLeft}
-              isRunning={isRunning}
-              onStart={handleStart}
-              onStop={handleStop}
-              onReset={handleReset}
-              addTime={addTime}
-            />
-          </PictureInPictureWindow>
+    <div className="grid grid-rows-[1fr,auto] min-h-screen">
+      <main className="grid items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+        <div>
+          <h1 className="font-bold text-5xl mb-4 text-center">
+            すみっこタイマー
+          </h1>
+          <div className="flex space-x-2 mb-3">
+            {!pipWindow ? (
+              <button className="btn btn-link" onClick={handleOpenPipWindow}>
+                画面のすみっこへGo!
+              </button>
+            ) : (
+              <button className="btn btn-link" onClick={handleClosePipWindow}>
+                もどって来ていいよ!
+              </button>
+            )}
+          </div>
+          <div
+            className="rounded-lg h-72 w-[400px] border shadow-lg"
+            ref={containerRef}
+          >
+            <PictureInPictureWindow pipWindow={pipWindow}>
+              <Timer
+                timeLeft={timeLeft}
+                isRunning={isRunning}
+                onStart={handleStart}
+                onStop={handleStop}
+                onReset={handleReset}
+                addTime={addTime}
+              />
+            </PictureInPictureWindow>
+          </div>
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="text-white text-center p-4">
+      <small>&copy; 2025 やまぴー from &quot;ZeroPlus&quot;</small>
+    </footer>
+  );
+}
